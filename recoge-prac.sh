bd4c12e018a604fbcf73684ad55de7f93cd9ecd8
DanielMenendezMira <daniel.menendez.mira@alumnos.upm.es>
2022-12-15 19:04:49 +0100

    arreglado msg log INFORMEPATH

--- recoge-prac.sh
+++ recoge-prac.sh
@@ -4,16 +4,16 @@
 
 #este script se encarga de recoger las prácticas
 INFORMEPATH=./informe-prac.log
-echo $(date) "[OK] - Parámetros recibidos -> (recoge-prac.sh)" rutaOrigen: $1 "|" rutaDestino: $2 >> /home/daniel/Escritorio/ASO/ScriptASO/informe-prac.log
+echo $(date) "[OK] - Parámetros recibidos -> (recoge-prac.sh)" rutaOrigen: $1 "|" rutaDestino: $2 >> $INFORMEPATH
 
 for directorio in $(find $1 -mindepth 1 -maxdepth 1 -type d)
 do
-	echo $(date) "[OK] - DENTRO FOR: -> (recoge-prac.sh)" $directorio $1 >> /home/daniel/Escritorio/ASO/ScriptASO/informe-prac.log
+	echo $(date) "[OK] - DENTRO FOR: -> (recoge-prac.sh)" $directorio $1 >> $INFORMEPATH
 	#obtener el nombre del directorio final
 	nombre="${directorio%/}"
 	nombre="${nombre##*/}"
 	
 	#movemos el fichero a la nueva ubicacion cambiandole el nombre
-	echo $(date) "[OK] - Práctica trasladada  -> (recoge-prac.sh)" Alumno: $nombre   Origen: $directorio/prac.sh   Destino: $2/$nombre.sh >> /home/daniel/Escritorio/ASO/ScriptASO/informe-prac.log
+	echo $(date) "[OK] - Práctica trasladada  -> (recoge-prac.sh)" Alumno: $nombre   Origen: $directorio/prac.sh   Destino: $2/$nombre.sh >> $INFORMEPATH
 	mv $directorio/prac.sh $2/$nombre.sh	
 done
