860e840d004b9990851f73cbd571eb11bda44ca0
DanielMenendezMira <daniel.menendez.mira@alumnos.upm.es>
2022-12-18 14:40:22 +0100

    TODO OK - Añadida barra progreso

--- gestiona-prac.sh
+++ gestiona-prac.sh
@@ -5,20 +5,20 @@ INFORMEPATH=./informe-prac.log
 PWD=$(pwd)
 menu(){
 
-    echo -e "\n";
+    echo -e "\n"
     echo ASO 22/23 - Practica 6
-    echo Daniel Menéndez Mira;
-    echo -e "\n";
-    echo Gestión de Prácticas;
-    echo pwd:$PWD #BORRAR ESTA LINEA
-    echo ----------------------;
-    echo -e "\n";
-    echo Menú;
-    echo    1')' Programar recogida de prácticas;
-    echo    2')' Empaquetado de prácticas de una asignatura;
-    echo    3')' Ver tamaño y fecha del ficehro de una asignatura;
-    echo    4')' Finalizar programa;
-    echo -e "\n";
+    echo Daniel Menéndez Mira
+    echo -e "\n"
+    echo "***********************************************"
+    echo "*           GESTIÓN DE PRÁCTICAS              *"
+    echo "***********************************************"
+    echo -e "\n"
+    echo Menú principal;
+    echo    1')' Programar recogida de prácticas
+    echo    2')' Empaquetado de prácticas de una asignatura
+    echo    3')' Ver tamaño y fecha del ficehro de una asignatura
+    echo    4')' Finalizar programa
+    echo -e "\n"
     read -p "Opción: " option
     
     case $option in 
@@ -32,9 +32,9 @@ menu(){
             menu3
             ;;
         4)
-            echo ***********************************************
-            echo *             FIN DEL PROGRAMA                *
-            echo ***********************************************
+            echo "***********************************************"
+            echo "*             FIN DEL PROGRAMA                *"
+            echo "***********************************************"
             sleep 1
             exit
             ;;
@@ -45,6 +45,7 @@ menu1(){
     existe=false #para comprobar si los directorios existen
     echo -e "\n"
     echo Menú 1 - Programar recogida de prácticas
+    echo ----------------------------------------
     echo -e "\n"
     read -p "Asignatura cuyas prácticas desea recoger: " asignatura
     #compruebo que el directorio de origen existe
@@ -119,6 +120,7 @@ menu1(){
 menu2(){
     echo -e "\n"
     echo Menú 2 - Empaquetar prácticas de la asignatura
+    echo ----------------------------------------------
     echo -e "\n"
     read -p "Asignatura cuyas prácticas desea empaquetar: " asignatura
     read -p "Ruta absoluta del directorio de prácticas: " rutaAbs
@@ -136,15 +138,16 @@ menu2(){
 	    	#Comprimo los ficheros existentes en el directorio especificado:   	
 	    	
 	    	#1.-obtener el nombre del directorio final
-		nombreDir="${rutaAbs%/}"
-		nombreDir="${nombreDir##*/}"
+		nombreDir="${rutaAbs%/}" #separo la ruta por los campos /
+		nombreDir="${nombreDir##*/}" #me quedo con el campo tras la ultima / para aislar el nombre del fichero
 		#2.-Añado al .log los parametros recibidos para la compresion
 	    	echo $(date) "[OK] - Parámetros recibidos: -> (gestiona-prac.sh)" Empaquetar asignatura $asignatura. Directorio de Origen: $rutaAbs. Nombre del ultimo directorio: $nombreDir >> $INFORMEPATH
 	    	#3.-Me muevo al directorio superior
 	    	cd $rutaAbs/.. ; 
 	    	#4.-Comprimo la carpeta completa para que al descomprimir no se desparramen todas las practicas
 	    	tar cfz $rutaAbs/$asignatura-$(date +%y%m%d).tgz $nombreDir
-	    	sleep 1 #Espero 1 segundo por si tardase más de la cuenta
+	    	tar cfz $rutaAbs/$asignatura-251222.tgz $nombreDir
+	    	barraProgreso #lo hacemos bonito
 	    	
 	    	echo $(date) "[OK] - Practica empaquetadas -> (gestiona-prac.sh)" Practicas de $asignatura empaquetadas en $rutaAbs >> $INFORMEPATH
 	    	echo [OK] se ha empaquetado el directorio $rutaAbs con las prácticas de la asignatura $asignatura 
@@ -161,19 +164,45 @@ menu2(){
 menu3(){
     echo -e "\n"
     echo Menú 3 - Obtener nombre y tamaño del fichero
+    echo --------------------------------------------
     echo -e "\n"
     read -p "Asignatura sobre la que queremos información: " asignatura
-    echo -e "\n"
-    echo La información obtenida de la asignatura empaquetada $asignatura es la siguiente:
-    #Hago cosas que me ha explicado Javi
-    for i in $(find ~ -regextype posix-egrep -regex "/[A-Za-z0-9/]*$asignatura-[0-9]{6}.tgz")
-    do
-    	tamano=$(stat -c%s "$i")
-    	echo -e " El fichero generado es \t$i y ocupa $tamano bytes"
-    done
+    barraProgreso
+    #devuelve true si y solo si la variable i tiene longitud distinta de 0 (es decir, si ha encontrado algo)
+    if [[ $(find ~ -regextype posix-egrep -regex "/[A-Za-z0-9/]*$asignatura-[0-9]{6}.tgz") ]] 
+    then
+    	echo La información obtenida de la asignatura empaquetada $asignatura es la siguiente:
+    	#repito la búsqueda por si hay varios .tgz de la misma asignatura (esto no es muy complejidad-friendly pero no sé hacerlo de otra forma)
+    	for i in $(find ~ -regextype posix-egrep -regex "/[A-Za-z0-9/]*$asignatura-[0-9]{6}.tgz")
+    	do 	
+	tamano=$(stat -c%s "$i") #obtengo el tamaño del fichero
+	nombre="${i%/}" #separo la ruta por los campos /
+	nombre="${nombre##*/}" #me quedo con el campo tras la ultima / para aislar el nombre del fichero
+		
+	echo -e "\n" El fichero generado es: $nombre y ocupa: $tamano bytes
+	echo -e El fichero se encuentra en $i
+	echo $(date) "[OK] - Realizo busqueda .tgz -> (gestiona-prac.sh)" El fichero generado es: $i y ocupa: $tamano bytes >> $INFORMEPATH
+    	done
+    else
+    	echo -e "\n"No se han encontrado ficheros .tgz de la asignatura $asignatura
+    fi    
+
     read -p "Pulse cualquier tecla para volver al menú principal"
+    menu
     
-    #SALIDA: El fichero generado es (por ejemplo): aso-221013.tgz y ocupa <n> bytes.
 }
 
+barraProgreso(){
+	echo -ne '[########-------------------------------------] (17%)\r'
+	sleep 0,5
+	echo -ne '[################-----------------------------] (33%)\r'
+	sleep 0,5
+	echo -ne '[#######################----------------------] (50%)\r'
+	sleep 0,5
+	echo -ne '[##############################---------------] (67%)\r'
+	sleep 0,5
+	echo -ne '[######################################-------] (84%)\r'
+	sleep 0,5
+	echo -ne '[#############################################] (100%)\r'
+}
 menu    #NO BORRAR ESTO
